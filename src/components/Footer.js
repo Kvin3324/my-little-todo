5b3edecd4af89a531bc10a5c7a5e1bc99b3dd919
Kévin Joya <kevin.joya@outlook.com>
2019-08-03 19:23:46 +0200

    App is completed

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+function Footer() {
+    return (
+        <footer>You need to do this... 💪 </footer>
+    )
+}
+
+export default Footer
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+function Header() {
+    return (
+        <header className="navbar">Todo list: </header>
+    )
+}
+
+export default Header
